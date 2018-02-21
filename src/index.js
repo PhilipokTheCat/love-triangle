6a24e3afc3449295c2209529664e128255112adc
unknown <philipok4596@gmail.com>
2018-02-22 02:19:23 +0300

    love-triangle/feature: implement love triangles count function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let firstPerson;
+  let secondPerson; 
+  let thirdPerson; 
+  let loveTrianglesCount = 0;
+  for (let i = 0; i < preferences.length; i++){
+  	try{
+	  	firstPerson = preferences[i];
+	  	secondPerson = preferences[firstPerson-1];
+	  	thirdPerson = preferences[secondPerson-1];
+	  }
+	catch(e){
+	 	continue;
+	}
+  	if ((thirdPerson-1 === i) && (firstPerson !== secondPerson) && (firstPerson !== thirdPerson)&& (thirdPerson !== secondPerson)) {
+  		loveTrianglesCount++;
+  		preferences[i] = null;
+  		preferences[firstPerson-1] = null;
+  		preferences[secondPerson-1] = null;
+  	}
+  };
+  return loveTrianglesCount;
 };
